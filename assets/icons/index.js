a1848361b1223465e877ec88a4d086fa3c806fc0
Aytac Malkoc <grand__ekinoks35@hotmail.com>
2021-05-01 15:22:23 +0300

    new icons added

---
+++ assets/icons/index.js
@@ -0,0 +1,9 @@
+import Explore from "./explore.svg";
+import Search from "./search.svg";
+import Favourites from "./favourites.svg";
+import Profile from "./profile.svg";
+import AddPhoto from "./add-photo.svg";
+import Photo from "./photo.svg";
+import Notification from "./notification.svg";
+
+export { Explore, Search, Favourites, Profile, AddPhoto, Photo, Notification };
